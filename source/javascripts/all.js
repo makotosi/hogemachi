789607a5814bd338b34aca9a3c22ded6a1f0734e
makotosiraisi <makotosiraisi3@gmail.com>
2016-12-06 21:50:32 +0900

    page scroller added

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+$(function() {
+  var offsetY = -10;
+  var time = 500;
+
+  $('a[href^=#]').click(function() {
+    var target = $(this.hash);
+    if (!target.length) return ;
+    var targetY = target.offset().top+offsetY;
+    $('html,body').animate({scrollTop: targetY}, time, 'swing');
+    window.history.pushState(null, null, this.hash);
+    return false;
+  });
+});
